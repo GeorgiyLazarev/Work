9db0fc47c7dcb5eeb17d80a492a61cab20deeb6c
Lazarev Georgiy <lazarev.georgiy9936@gmail.com>
2026-03-15 19:53:23 +0400

    Выполнение домашнего задания №1

---
+++ src/homework1/Task1.java
@@ -0,0 +1,19 @@
+package homework1;
+
+/**
+ Задача 1:
+ Написать приложение, которое будет вычислять и выводить значение по
+ формуле: a=4*(b+c-1)/2;
+ b и c задаем в коде самостоятельно.
+ */
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        double b = 6.2;
+        double c = 54.4;
+        double a = 4*(b + c - 1)/2;
+
+        System.out.println("4*(" + b + " + " + c + " - 1)/2 = " + a);
+    }
+}
