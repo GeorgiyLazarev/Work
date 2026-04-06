7cc1135562a2f385acb3dcf17e675fa58b1a7919
Lazarev Georgiy <lazarev.georgiy9936@gmail.com>
2026-04-06 15:33:10 +0400

    Добавление решения задачек 4 и 5 из домашки обработки строк

--- src/homework7/ScannerClass.java
+++ src/homework7/ScannerClass.java
@@ -1,9 +1,9 @@
 package homework7;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class ScannerClass {
+    //Метод создания 3 массивов через ввод с консоли
     public static String[] scannerVoid() {
         Scanner scanner = new Scanner(System.in);
         String[] massive = new String[3];
@@ -19,5 +19,32 @@ public class ScannerClass {
         System.out.println("----------");
 
         return massive;
+    }
+
+    //Метод сравнения символов
+    public static boolean hasUniqueChars(String word) {
+        boolean thereNoMatch = false;
+        boolean coincidence = true;
+
+        for (int i = 0; i < word.length(); i++) {
+            for (int j = i + 1; j < word.length(); j++) {
+                if (word.charAt(i) == word.charAt(j)) {
+                    return thereNoMatch;
+                }
+            }
+        }
+        return coincidence;
+    }
+
+    //Метод создания дубликатов каждой буквы строки
+    public static String duplicate(String str) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            char j = str.charAt(i);
+            result.append(j).append(j);
+        }
+
+        return result.toString();
     }
 }
--- src/homework7/Task4.java
+++ src/homework7/Task4.java
@@ -1,10 +1,38 @@
 package homework7;
 
+import java.util.Arrays;
+
 /**
  * Задача 4:
  * Ввести 3 строки с консоли. Найти слово, состоящее только из различных
  * символов. Если таких слов несколько, найти первое из них.
  */
 
-public class Task4 {
+public class Task4 extends ScannerClass {
+    public static void main(String[] args) {
+        String[] myMassive = scannerVoid();
+        System.out.println(Arrays.toString(myMassive));
+
+        boolean hello = false;
+        int number = 0;
+        int numberMassive = 1;
+        for (String s : myMassive) {
+            String[] newMassive = s.split("[ ,]+");
+            System.out.printf("Массив №%d %s%n", numberMassive, Arrays.toString(newMassive));
+            number++;
+            numberMassive++;
+
+            for (String uniqueness : newMassive) {
+                if (hasUniqueChars(uniqueness)) {
+                    System.out.printf("Нашли слово \"%s\" подходящее под условие в строке %d%n", uniqueness, number);
+                    hello = true;
+                    break;
+                }
+            }
+        }
+
+        if (!hello) {
+            System.out.println("Слов с уникальными символами не найдено");
+        }
+    }
 }
